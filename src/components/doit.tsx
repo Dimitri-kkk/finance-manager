dd8039658482fd12fcbf83d3c8598fabef3df54c
tamta-kinkladze <tamta.qinqladze.6@gmail.com>
2024-11-25 21:47:33 +0400

    feat: components

--- src/components/doit.tsx
+++ src/components/doit.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const doit = () => {
   return (
-    <div>doit for your self and your loved ones.</div>
-  )
-}
+    <div>
+      doit for your self and your loved ones.
+      <h1>dsads</h1>
+    </div>
+  );
+};
 
-export default doit
+export default doit;
