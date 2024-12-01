2b74b02bf3bdfc518cd3e1b91258ba56fa356d3d
dimitri <dyaryarashvili@gmail.com>
2024-12-01 21:39:54 +0400

    feat: streak

--- src/components/streak.tsx
+++ src/components/streak.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const streak = () => {
   return (
-    <div className="flex flex-1 mt-2">
-        <p>streak 1</p>
+    <div className="flex flex-1 mt-2 streaki">
+        <p>streak 4</p>
     </div>
   )
 }
