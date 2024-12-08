f5c9a09a197b34b71ef3e04e9321cba17c8837d1
dimitri <dyaryarashvili@gmail.com>
2024-12-09 01:01:32 +0400

    feat

--- src/components/more.tsx
+++ src/components/more.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const more = () => {
   return (
     <div className="container-area">more contenti
-    <p>aba ise rogor</p>
+    <p>aba ise rogora</p>
     </div>
   )
 }
