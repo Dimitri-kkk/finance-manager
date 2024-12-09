3cdcc7aaf9142fe72b106d0e812d45a2a53681fd
dimitri <dyaryarashvili@gmail.com>
2024-12-09 21:27:38 +0400

    feat

--- src/components/active.tsx
+++ src/components/active.tsx
@@ -7,6 +7,7 @@ const Active = () => {
         <h2 className="h3-bold">Good fortune is on its way.</h2>
         <h3 className="h3-bold mt-2">In 4 months you will have a job and good salary with great future.</h3>
         <h4>You are going to shine</h4>
+        <p>You are slowly but surely going there. be persistent</p>
     </div>
   )
 }
