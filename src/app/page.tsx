262290ba9b83097d6ec0ff9ecff47328e3329c32
dimitri <dyaryarashvili@gmail.com>
2024-12-09 21:28:08 +0400

    feat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <div>
       <h1>Home</h1>
       <div className="flex flex-col flex-1">
-        <h3>gugugagaaa</h3>
+        <h3>gugugagaaagaga</h3>
       </div>
     </div>
 
