dbc13837bd00df326a42d4d03fc92f7adb3288c2
dimitri <dyaryarashvili@gmail.com>
2024-12-10 19:31:01 +0400

    feat

--- src/components/doit.tsx
+++ src/components/doit.tsx
@@ -5,7 +5,7 @@ const doit = () => {
     <div>
       doit for your self and your loved ones.
       do it otherwise nothing matters.
-      <p> be strong </p>
+      <p> be strong pls</p>
     </div>
   );
 };
