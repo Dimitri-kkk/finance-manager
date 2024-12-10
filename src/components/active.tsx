6f58f63f22a1d179fc5f66bbce61b938daf9c045
dimitri <dyaryarashvili@gmail.com>
2024-12-10 19:30:49 +0400

    feat

--- src/components/active.tsx
+++ src/components/active.tsx
@@ -8,6 +8,7 @@ const Active = () => {
         <h3 className="h3-bold mt-2">In 4 months you will have a job and good salary with great future.</h3>
         <h4>You are going to shine</h4>
         <p>You are slowly but surely going there. be persistent</p>
+        <p>In maximum 4 months you will land your first job as a developer and become successful like u never imagined</p>
     </div>
   )
 }
