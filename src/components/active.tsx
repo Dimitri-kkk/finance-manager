b1ccc1136046e72c17b9d5698009745b8d2b82d7
dimitri <dyaryarashvili@gmail.com>
2024-12-11 23:55:40 +0400

    feat

--- src/components/active.tsx
+++ src/components/active.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Active = () => {
   return (
-    <div className="flex flex-1 mt-2">
+    <div className="flex flex-1 mt-2 future goal">
         <p>keep going, you are almost there. just a bit more</p>
         <h2 className="h3-bold">Good fortune is on its way.</h2>
         <h3 className="h3-bold mt-2">In 4 months you will have a job and good salary with great future.</h3>
